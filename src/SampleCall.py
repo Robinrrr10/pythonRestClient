58125a0cf1d4d9ef4c9e5e6476c2ca35aca0bfbe
robinrrr10 <robinrrr10@gmail.com>
2019-11-18 23:45:11 +0530

    first commit of python rest client demo project

---
+++ src/SampleCall.py
@@ -0,0 +1,31 @@
+import requests
+
+print("Goodbye, World!")
+class SampleRestClient():
+    def makeGetCall(self):
+        print("Making GET Rest call")
+        urlwithpath = "https://reqres.in/api/user/1"
+        print("Trying to hit with url:", urlwithpath)
+        response = requests.get(url = urlwithpath)
+        print("Body is:", response.content)
+        print("body 2 is", response.text)
+        responseJson = response.json()
+        print("Response is:", responseJson)
+        print("new id is", responseJson['data']['name'])
+        
+    def makePostCall(self):
+        print("Making POST Rest call")
+        urlwithpath = "https://reqres.in/api/users"
+        requestBody = {"name": "morpheus333","job": "leader"}
+        response = requests.post(url = urlwithpath, data = requestBody)
+        print("Body is:", response.content)
+        print("body 2 is", response.text)
+        responseJson = response.json()
+        print("Response is:", responseJson)
+        print("new id is", responseJson['id'])
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+client = SampleRestClient()
+client.makeGetCall()
+client.makePostCall()
+   
